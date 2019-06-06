7f6ec3b0adde8c7d4fb15f32799e2de3886378f5
Xing Gao <18340825824@163.com>
2019-06-06 17:02:54 +0800

    二进制中1的个数

---
+++ binaryOperation/lowbit.go
@@ -0,0 +1,21 @@
+package binaryOperation
+
+import "fmt"
+
+func lowbit(x int) int {
+	return x & -x
+}
+
+func getCount(x int) int {
+	//给定一个长度为n的数列,请你求出数列中每个数的二进制表示中1的个数
+	res := 0
+	for x > 0 {
+		x -= lowbit(x)
+		res++
+	}
+	return res
+}
+
+func main() {
+	fmt.Print(getCount(15))
+}

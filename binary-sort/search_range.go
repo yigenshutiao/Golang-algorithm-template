b8fd03cd64a3abb1312a110c835f9de6f7c107e4
Xing Gao <18340825824@163.com>
2021-11-06 14:21:33 +0800

    二分增加注释

--- binary-sort/search_range.go
+++ binary-sort/search_range.go
@@ -2,12 +2,14 @@ package main
 
 import "fmt"
 
-// 此问题用到两种二分模板来寻找边界
+// BinarySearch 此问题用到两种二分模板来寻找边界
 // 题目，在一个单调递增的序列里找到一个数字的起始、终止坐标，找不到返回[-1,-1]
-func searchRange(num []int, target int) []int {
+func BinarySearch(num []int, target int) []int {
 	if len(num) == 0 {
 		return []int{-1, -1}
 	}
+
+	// 先寻找起点，即左边界
 	l, r := 0, len(num)-1
 	for l < r {
 		mid := (l + r) >> 1
@@ -19,22 +21,26 @@ func searchRange(num []int, target int) []int {
 	}
 	if num[l] != target {
 		return []int{-1, -1}
-	} else {
-		start := l
-		l, r := 0, len(num)-1
-		for l < r {
-			mid := (l + r + 1) >> 1
-			if num[mid] <= target {
-				l = mid
-			} else {
-				r = mid - 1
-			}
-		}
-		end := r
-		return []int{start, end}
 	}
+	start := l
+
+	// 寻找终点，即右边界
+	l, r = 0, len(num)-1
+	for l < r {
+		// 当 l = r - 1  时，mid 若不加1，则等于l，区间为[l, r], 没有变化, 造成死循环
+		// 只有 +1,才能保证mid = r，跳出终止条件
+		mid := (l + r + 1) >> 1
+		if num[mid] <= target {
+			l = mid
+		} else {
+			r = mid - 1
+		}
+	}
+	end := r
+	return []int{start, end}
+
 }
 
 func main() {
-	fmt.Println(searchRange([]int{1, 2, 4, 4, 5, 8, 9}, 7))
+	fmt.Println(BinarySearch([]int{1, 2, 4, 4, 5, 8, 9}, 4))
 }

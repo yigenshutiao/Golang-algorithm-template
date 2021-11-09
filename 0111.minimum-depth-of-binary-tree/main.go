051da93319649edfe6368afc5372e8d3907b5f53
Xing Gao <18340825824@163.com>
2021-11-10 00:18:09 +0800

    111. 二叉树的最小深度 增加bfs解决方法

--- 0111.minimum-depth-of-binary-tree/main.go
+++ 0111.minimum-depth-of-binary-tree/main.go
@@ -24,6 +24,12 @@ func dfs(root *util.TreeNode, cur int, res *int) {
 	dfs(root.Right, cur, res)
 }
 
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
 func minDepth(root *util.TreeNode) int {
 	if root == nil {
 		return 0
@@ -33,6 +39,32 @@ func minDepth(root *util.TreeNode) int {
 	dfs(root, 0, &res)
 
 	return res
+}
+
+func minDepthBfs(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	queue := []*TreeNode{}
+	count := []int{}
+	queue = append(queue, root)
+	count = append(count, 1)
+	for i := 0; i < len(queue); i++ {
+		node := queue[i]
+		depth := count[i]
+		if node.Left == nil && node.Right == nil {
+			return depth
+		}
+		if node.Left != nil {
+			queue = append(queue, node.Left)
+			count = append(count, depth+1)
+		}
+		if node.Right != nil {
+			queue = append(queue, node.Right)
+			count = append(count, depth+1)
+		}
+	}
+	return 0
 }
 
 func main() {

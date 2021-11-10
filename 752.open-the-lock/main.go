d11767f18c0da45161480519d8e8d098d93bf198
gaoxing <richardgaoxing@didichuxing.com>
2021-11-10 13:54:12 +0800

    752. open-the-lock

---
+++ 752.open-the-lock/main.go
@@ -0,0 +1,60 @@
+package main
+
+func openLock(deadends []string, target string) int {
+	const start = "0000"
+	if target == start {
+		return 0
+	}
+
+	dead := map[string]bool{}
+	for _, s := range deadends {
+		dead[s] = true
+	}
+	if dead[start] {
+		return -1
+	}
+
+	// 枚举 status 通过一次旋转得到的数字
+	get := func(status string) (ret []string) {
+		s := []byte(status)
+		for i, b := range s {
+			s[i] = b - 1
+			if s[i] < '0' {
+				s[i] = '9'
+			}
+			ret = append(ret, string(s))
+			s[i] = b + 1
+			if s[i] > '9' {
+				s[i] = '0'
+			}
+			ret = append(ret, string(s))
+			s[i] = b
+		}
+		return
+	}
+
+	type pair struct {
+		status string
+		step   int
+	}
+	q := []pair{{start, 0}}
+	seen := map[string]bool{start: true}
+	for len(q) > 0 {
+		p := q[0]
+		q = q[1:]
+		for _, nxt := range get(p.status) {
+			if !seen[nxt] && !dead[nxt] {
+				if nxt == target {
+					return p.step + 1
+				}
+				seen[nxt] = true
+				q = append(q, pair{nxt, p.step + 1})
+			}
+		}
+	}
+	return -1
+}
+
+func main() {
+
+}

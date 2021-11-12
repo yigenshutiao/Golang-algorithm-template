7e1fe82ee3d676ec546aad4e18614e572895f09e
gaoxing <richardgaoxing@didichuxing.com>
2021-11-12 13:31:16 +0800

    23. merge k sorted lists

---
+++ 0023.merge-k-sorted-lists/merge-k-sorted-lists.go
@@ -0,0 +1,40 @@
+package _023_merge_k_sorted_lists
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeKLists(lists []*ListNode) *ListNode {
+	if len(lists) == 0 {
+		return nil
+	}
+
+	if len(lists) == 1 {
+		return lists[0]
+	}
+
+	return mergeTwoLists(mergeKLists(lists[:len(lists)/2]), mergeKLists(lists[len(lists)/2:]))
+}
+
+func mergeTwoLists(l1, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+
+	if l2 == nil {
+		return l1
+	}
+
+	head := &ListNode{}
+
+	if l1.Val <= l2.Val {
+		head = l1
+		head.Next = mergeTwoLists(l1.Next, l2)
+	} else {
+		head = l2
+		head.Next = mergeTwoLists(l1, l2.Next)
+	}
+
+	return head
+}

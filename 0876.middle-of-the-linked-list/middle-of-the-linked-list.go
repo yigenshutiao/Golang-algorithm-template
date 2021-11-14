04d4bf145f07154b9d8f6d6a7d16228b1be75762
Xing Gao <18340825824@163.com>
2021-11-14 17:38:05 +0800

    876. 链表的中间结点,增加注释

--- 0876.middle-of-the-linked-list/middle-of-the-linked-list.go
+++ 0876.middle-of-the-linked-list/middle-of-the-linked-list.go
@@ -2,14 +2,6 @@ package _876_middle_of_the_linked_list
 
 import "github.com/yigenshutiao/Golang-algorithm-template/util"
 
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
-
 type ListNode = util.ListNode
 
 func middleNode(head *ListNode) *ListNode {
@@ -17,6 +9,7 @@ func middleNode(head *ListNode) *ListNode {
 	one, second := head, head
 
 	// second != nil 为了判断快指针走到末尾的情况
+	// 当second 已经走到末尾的nil, 若再判断second的Next，就会panic
 	for second != nil && second.Next != nil {
 		one = one.Next
 		second = second.Next.Next

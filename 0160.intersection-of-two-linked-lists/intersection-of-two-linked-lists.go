6fdd7e3c2d5384e1fb3f33db063cb3b03cdf8d28
Xing Gao <18340825824@163.com>
2021-11-16 00:04:13 +0800

    160. 增加注释

--- 0160.intersection-of-two-linked-lists/intersection-of-two-linked-lists.go
+++ 0160.intersection-of-two-linked-lists/intersection-of-two-linked-lists.go
@@ -6,8 +6,9 @@ type ListNode = util.ListNode
 
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
 	p1, p2 := headA, headB
-
 	for p1 != p2 {
+		// 这里需要用p1 == nil 做判断，不能用p1.Next
+		// 用 p1.Next 会重复
 		if p1 == nil {
 			p1 = headB
 		} else {
--- 0160.intersection-of-two-linked-lists/intersection-of-two-linked-lists_test.go
+++ 0160.intersection-of-two-linked-lists/intersection-of-two-linked-lists_test.go
@@ -1,27 +1,63 @@
 package _160_intersection_of_two_linked_lists
 
 import (
-	"reflect"
+	"github.com/stretchr/testify/assert"
+	"github.com/yigenshutiao/Golang-algorithm-template/util"
 	"testing"
 )
 
+// tcs is testcase slice
+var tcs = []struct {
+	a, b   []int
+	ea, eb int
+}{
+
+	{
+		[]int{4, 1, 8, 4, 5},
+		[]int{5, 0, 1, 8, 4, 5},
+		2,
+		3,
+	},
+
+	//{
+	//	[]int{0, 9, 1, 2, 4},
+	//	[]int{3, 2, 4},
+	//	3,
+	//	1,
+	//},
+	//
+	//{
+	//	[]int{2, 6, 4},
+	//	[]int{1, 5},
+	//	3,
+	//	2,
+	//},
+
+	// 可以有多个 testcase
+}
+
+// head must Not be nil
+func tailOf(head *ListNode) *ListNode {
+	for head.Next != nil {
+		head = head.Next
+	}
+	return head
+}
+
 func Test_getIntersectionNode(t *testing.T) {
-	type args struct {
-		headA *ListNode
-		headB *ListNode
-	}
-	tests := []struct {
-		name string
-		args args
-		want *ListNode
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := getIntersectionNode(tt.args.headA, tt.args.headB); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("getIntersectionNode() = %v, want %v", got, tt.want)
-			}
-		})
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		tail := util.Ints2List(tc.a[tc.ea:])
+
+		headA := util.Ints2List(tc.a[:tc.ea])
+		tailA := tailOf(headA)
+		tailA.Next = tail
+
+		headB := util.Ints2List(tc.b[:tc.eb])
+		tailB := tailOf(headB)
+		tailB.Next = tail
+
+		ast.Equal(tail, getIntersectionNode(headA, headB), "输入:%v", tc)
 	}
 }

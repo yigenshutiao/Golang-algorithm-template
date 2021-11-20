e7c595f2a12a99731e0dc94fa59a4f8ddda1de6b
Xing Gao <18340825824@163.com>
2021-11-20 17:14:07 +0800

    34. 在排序数组中查找元素的第一个和最后一个位置  补个单测

--- 0034.find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array_test.go
+++ 0034.find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array_test.go
@@ -1,16 +1,19 @@
 package _034_find_first_and_last_position_of_element_in_sorted_array
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func Test_rightSearch(t *testing.T) {
+func Test_searchRange(t *testing.T) {
 	type args struct {
-		num    []int
+		nums   []int
 		target int
 	}
 	tests := []struct {
 		name string
 		args args
-		want int
+		want []int
 	}{
 		{
 			"test 1",
@@ -18,13 +21,13 @@ func Test_rightSearch(t *testing.T) {
 				[]int{5, 7},
 				7,
 			},
-			2,
+			[]int{1, 1},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := rightSearch(tt.args.num, tt.args.target); got != tt.want {
-				t.Errorf("rightSearch() = %v, want %v", got, tt.want)
+			if got := searchRange(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("searchRange() = %v, want %v", got, tt.want)
 			}
 		})
 	}

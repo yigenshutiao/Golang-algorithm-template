7152ca4aed4b228f1ec18804304ba6ca9733d384
Xing Gao <18340825824@163.com>
2021-11-21 23:30:28 +0800

    76. 最小覆盖子串 加代码注释

---
+++ 0076.minimum-window-substring/minimum-window-substring_test.go
@@ -0,0 +1,32 @@
+package _076_minimum_window_substring
+
+import "testing"
+
+func Test_minWindow(t *testing.T) {
+	type args struct {
+		s string
+		t string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			"test1",
+			args{
+				"XABAACBAB",
+				"ABC",
+			},
+			"ACB",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minWindow(tt.args.s, tt.args.t); got != tt.want {
+				t.Errorf("minWindow() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

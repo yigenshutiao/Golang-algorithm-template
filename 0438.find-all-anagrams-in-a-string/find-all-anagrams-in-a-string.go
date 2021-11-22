209869a87d93b2a4fe8d3dbd810b3156ff7be748
gaoxing <richardgaoxing@didichuxing.com>
2021-11-22 13:53:30 +0800

    438. 找到字符串中所有字母异位词

---
+++ 0438.find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.go
@@ -0,0 +1,44 @@
+package _438_find_all_anagrams_in_a_string
+
+//输入: s = "cbae", p = "abc"
+//输出: [0,6]
+
+func findAnagrams(s string, p string) []int {
+	pl := len(p)
+	sl := len(s)
+	if pl > sl {
+		return nil
+	}
+	var result []int
+
+	m := make(map[byte]int)
+	for i := 0; i < pl; i++ {
+		m[p[i]]++
+	}
+
+	for i1 := 0; i1 < pl; i1++ {
+		m[s[i1]]--
+	}
+
+	for i := 0; i < sl-pl+1; i++ {
+
+		flag := 0
+
+		if i > 0 {
+			m[s[i-1]]++
+			m[s[i+pl-1]]--
+		}
+
+		for _, v := range m {
+			if v != 0 {
+				flag = 1
+				break
+			}
+		}
+		if flag == 0 {
+			result = append(result, i)
+		}
+
+	}
+	return result
+}
---
+++ 0438.find-all-anagrams-in-a-string/find-all-anagrams-in-a-string_test.go
@@ -0,0 +1,34 @@
+package _438_find_all_anagrams_in_a_string
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_findAnagrams(t *testing.T) {
+	type args struct {
+		s string
+		p string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			"test 1",
+			args{
+				"abaacbabc",
+				"abc",
+			},
+			[]int{0, 1, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findAnagrams(tt.args.s, tt.args.p); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findAnagrams() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

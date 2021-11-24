7a1a1059a6e2381bd27d2566dd5d150b7c9a4325
gaoxing <richardgaoxing@didichuxing.com>
2021-11-24 13:45:28 +0800

    743. 网络延迟时间 晚上回去研究一下

--- 0743.network-delay-time/network-delay-time.go
+++ 0743.network-delay-time/network-delay-time.go
@@ -65,3 +65,59 @@ func networkDelayTime(times [][]int, n int, k int) int {
 	}
 	return max
 }
+
+func networkDelayTimes(times [][]int, n, k int) (ans int) {
+	const inf = math.MaxInt64 / 2
+	g := make([][]int, n)
+	for i := range g {
+		g[i] = make([]int, n)
+		for j := range g[i] {
+			g[i][j] = inf
+		}
+	}
+	for _, t := range times {
+		x, y := t[0]-1, t[1]-1
+		g[x][y] = t[2]
+	}
+
+	dist := make([]int, n)
+	for i := range dist {
+		dist[i] = inf
+	}
+	dist[k-1] = 0
+	used := make([]bool, n)
+	for i := 0; i < n; i++ {
+		x := -1
+		for y, u := range used {
+			if !u && (x == -1 || dist[y] < dist[x]) {
+				x = y
+			}
+		}
+		used[x] = true
+		for y, time := range g[x] {
+			dist[y] = min(dist[y], dist[x]+time)
+		}
+	}
+
+	for _, d := range dist {
+		if d == inf {
+			return -1
+		}
+		ans = max(ans, d)
+	}
+	return
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}

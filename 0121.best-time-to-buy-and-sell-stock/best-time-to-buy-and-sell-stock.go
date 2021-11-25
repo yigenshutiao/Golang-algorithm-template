ebdc630307277b562d00b096425e06961db84a42
Xing Gao <18340825824@163.com>
2021-11-26 00:11:19 +0800

    121. 买卖股票的最佳时机 增加注释

--- 0121.best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.go
+++ 0121.best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.go
@@ -1,15 +1,16 @@
 package _121_best_time_to_buy_and_sell_stock
 
 func maxProfit(prices []int) int {
-	dp := [][]int{}
+	dp := make([][]int, len(prices))
 
 	for i := 0; i < len(prices); i++ {
 		dp[i] = make([]int, 2)
 	}
 
 	dp[0][0] = 0
-	// dp[0][1] = -prices[0] 这一行是干什么的...
-	dp[0][1] = -999
+	// dp[i][1] = max(dp[i-1][1], dp[i-1][0] - price[i]) dp[i-1][0] 为0，忽略
+	// dp[0][1] = -prices[0]
+	dp[0][1] = -prices[0]
 
 	for i := 1; i < len(prices); i++ {
 		dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])

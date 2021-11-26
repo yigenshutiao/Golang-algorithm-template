793c35f8a4c3c21f75a4dc98f9475ee25972ccec
Xing Gao <18340825824@163.com>
2021-11-26 10:04:22 +0800

    123. 买卖股票的最佳时机 III

---
+++ best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.go
@@ -0,0 +1,33 @@
+package best_time_to_buy_and_sell_stock_iii
+
+func maxProfit(prices []int) int {
+	dp := make([][][]int, len(prices))
+	for i := 0; i < len(prices); i++ {
+		dp[i] = make([][]int, 3)
+		for j := 0; j <= 2; j++ {
+			dp[i][j] = make([]int, 3)
+		}
+	}
+
+	for i := 0; i < len(prices); i++ {
+		for j := 2; j >= 1; j-- {
+			if i == 0 {
+				dp[i][j][0] = 0
+				dp[i][j][1] = -prices[i]
+				continue
+			}
+
+			dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j][1]+prices[i])
+			dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j-1][0]-prices[i])
+		}
+	}
+
+	return dp[len(prices)-1][2][0]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
---
+++ best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii_test.go
@@ -0,0 +1,29 @@
+package best_time_to_buy_and_sell_stock_iii
+
+import "testing"
+
+func Test_maxProfit(t *testing.T) {
+	type args struct {
+		prices []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"test 1",
+			args{
+				prices: []int{3, 3, 5, 0, 0, 3, 1, 4},
+			},
+			6,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxProfit(tt.args.prices); got != tt.want {
+				t.Errorf("maxProfit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

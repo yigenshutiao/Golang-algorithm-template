5f6246d870637493f487df908b0ce2881afe188f
Xing Gao <18340825824@163.com>
2021-11-28 22:58:53 +0800

    188. 买卖股票的最佳时机 IV

--- 0188.best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.go
+++ 0188.best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.go
@@ -2,12 +2,15 @@ package _188_best_time_to_buy_and_sell_stock_iv
 
 // k = 2, prices = [3,2,6,5,0,3]
 func maxProfit(k int, prices []int) int {
-	dp := make([][][]int, len(prices))
+	if len(prices) <= 1 {
+		return 0
+	}
 
+	dp := make([][][]int, len(prices))
 	for i := 0; i < len(prices); i++ {
 		dp[i] = make([][]int, k+1)
 		for j := k; j >= 0; j-- {
-			dp[i][j] = make([]int, 2)
+			dp[i][j] = make([]int, 3)
 		}
 	}
 
@@ -19,13 +22,13 @@ func maxProfit(k int, prices []int) int {
 	for i := 0; i < len(prices); i++ {
 		for j := k; j >= 1; j-- {
 			if i == 0 {
-				dp[0][k][0] = 0
-				dp[0][k][1] = -prices[i]
+				dp[i][j][0] = 0
+				dp[i][j][1] = -prices[i]
 				continue
 			}
 
-			dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k][1]+prices[i])
-			dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0]-prices[i])
+			dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j][1]+prices[i])
+			dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j-1][0]-prices[i])
 		}
 	}
 

f209b3e3d685091d18b143819f91c989f910a502
Xing Gao <18340825824@163.com>
2021-11-28 23:12:45 +0800

    309. 最佳买卖股票时机含冷冻期

---
+++ 0309.best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.go
@@ -0,0 +1,36 @@
+package _309_best_time_to_buy_and_sell_stock_with_cooldown
+
+//input [1,2,3,0,2]
+func maxProfit(prices []int) int {
+	dp := make([][]int, len(prices))
+	for i := 0; i < len(prices); i++ {
+		dp[i] = make([]int, 2)
+	}
+
+	dp[0][0] = 0
+	dp[0][1] = -prices[0]
+
+	for i := 0; i < len(prices); i++ {
+		if i == 0 {
+			dp[i][0] = 0
+			dp[i][1] = -prices[0]
+			continue
+		}
+
+		if i == 1 {
+			dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+			dp[i][1] = max(dp[i-1][1], -prices[i])
+			continue
+		}
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+		dp[i][1] = max(dp[i-1][1], dp[i-2][0]-prices[i])
+	}
+	return dp[len(prices)-1][0]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}

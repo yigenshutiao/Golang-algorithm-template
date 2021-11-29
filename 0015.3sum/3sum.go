2e27817a070efde4c7c776b494489ec5ca822257
gaoxing <richardgaoxing@didichuxing.com>
2021-11-29 14:04:46 +0800

    15. 三数之和 加点注释

--- 0015.3sum/3sum.go
+++ 0015.3sum/3sum.go
@@ -8,10 +8,12 @@ func threeSum(nums []int) [][]int {
 
 	for i := 0; i < len(nums)-2; i++ {
 		n1 := nums[i]
+		// 这里是个优化，最小的数都大于0，相当于无解，直接退出
 		if n1 > 0 {
 			break
 		}
-		if i > 0 && n1 == nums[i-1] {
+		// 这里是用来去重的，如果num[i] == num[i-1], 跳过
+		if i > 0 && nums[i] == nums[i-1] {
 			continue
 		}
 		l, r := i+1, len(nums)-1
@@ -19,9 +21,11 @@ func threeSum(nums []int) [][]int {
 			n2, n3 := nums[l], nums[r]
 			if n1+n2+n3 == 0 {
 				res = append(res, []int{n1, n2, n3})
+				// 这里是要跳过相同的元素
 				for l < r && nums[l] == n2 {
 					l++
 				}
+				// 这里是要跳过相同的元素
 				for l < r && nums[r] == n3 {
 					r--
 				}
---
+++ 0015.3sum/3sum_test.go
@@ -0,0 +1,32 @@
+package _015_3sum
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_threeSum(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			"test 1",
+			args{
+				[]int{-1, 0, 1, 2, -1, -4},
+			},
+			[][]int{{-1, -1, 2}, {-1, 0, 1}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := threeSum(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("threeSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

c174e1f7917550f86e2c036313c4f02c8734f093
Xing Gao <18340825824@163.com>
2021-11-30 23:21:52 +0800

    213. 打家劫舍 II 加点注释

--- 0213.house-robber-ii/house-robber-ii.go
+++ 0213.house-robber-ii/house-robber-ii.go
@@ -15,9 +15,12 @@ func rob(nums []int) int {
 		return max(nums[0], nums[1])
 	}
 
+	// 偷了第一间，不能偷最后一间
 	res1 := robs(nums[0 : len(nums)-1])
+	// 偷了最后一间，不能偷第一间
 	res2 := robs(nums[1:len(nums)])
 
+	// 所以两种方案都试试，最后选择利润高的方案
 	return max(res1, res2)
 }
 func robs(nums []int) int {

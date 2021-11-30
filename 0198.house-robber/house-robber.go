f0e0cb0d0a65f05930794f8eb774fca1b14e22a7
Xing Gao <18340825824@163.com>
2021-11-30 17:16:26 +0800

    198. 打家劫舍

--- 0198.house-robber/house-robber.go
+++ 0198.house-robber/house-robber.go
@@ -2,21 +2,24 @@ package _198_house_robber
 
 //input [2,7,9,3,1]
 func rob(nums []int) int {
-	dp := make([]int, len(nums))
-
+	if len(nums) == 2 {
+		return max(nums[0], nums[1])
+	}
+	dp := make([]int, len(nums)+1)
+	dp[0] = 0
 	for i := 0; i < len(nums); i++ {
 		if i == 0 {
-			dp[0] = nums[0]
+			dp[1] = nums[0]
 			continue
 		}
 		if i == 1 {
-			dp[1] = nums[1]
-			continue
+			dp[2] = max(dp[0], nums[i])
 		}
-		dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+
+		dp[i+1] = max(dp[i], dp[i-1]+nums[i])
 	}
 
-	return dp[len(nums)-1]
+	return dp[len(nums)]
 }
 
 func max(a, b int) int {
---
+++ 0198.house-robber/house-robber_test.go
@@ -0,0 +1,29 @@
+package _198_house_robber
+
+import "testing"
+
+func Test_rob(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"test1",
+			args{
+				[]int{2, 7, 9, 3, 1},
+			},
+			12,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rob(tt.args.nums); got != tt.want {
+				t.Errorf("rob() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

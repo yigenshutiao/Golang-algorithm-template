5f810493d41b7aee6b0f9a1e0cf6f5003b28d903
Xing Gao <18340825824@163.com>
2021-12-01 16:37:00 +0800

    337. 打家劫舍 III

---
+++ 0337.house-robber-iii/house-robber-iii.go
@@ -0,0 +1,29 @@
+package _337_house_robber_iii
+
+import "github.com/yigenshutiao/Golang-algorithm-template/util"
+
+type TreeNode = util.TreeNode
+
+func rob(root *TreeNode) int {
+	val := dfs(root)
+	return max(val[0], val[1])
+}
+
+func dfs(root *TreeNode) []int {
+	if root == nil {
+		return []int{0, 0}
+	}
+
+	left, right := dfs(root.Left), dfs(root.Right)
+	choose := root.Val + left[1] + right[1]
+	notChoose := max(left[0], left[1]) + max(right[0], right[1])
+
+	return []int{choose, notChoose}
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}

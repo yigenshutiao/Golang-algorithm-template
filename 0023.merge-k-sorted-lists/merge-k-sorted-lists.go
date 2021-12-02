564355dbd05681450ff51dcc22aad0e2cef27e8c
gaoxing <richardgaoxing@didichuxing.com>
2021-12-02 13:47:47 +0800

    23. 合并K个升序链表 换一种更好理解的解法

--- 0023.merge-k-sorted-lists/merge-k-sorted-lists.go
+++ 0023.merge-k-sorted-lists/merge-k-sorted-lists.go
@@ -13,28 +13,34 @@ func mergeKLists(lists []*ListNode) *ListNode {
 	if len(lists) == 1 {
 		return lists[0]
 	}
+	left := lists[0 : len(lists)/2]
+	right := lists[len(lists)/2:]
 
-	return mergeTwoLists(mergeKLists(lists[:len(lists)/2]), mergeKLists(lists[len(lists)/2:]))
+	return mergeTwoLists(mergeKLists(left), mergeKLists(right))
 }
 
-func mergeTwoLists(l1, l2 *ListNode) *ListNode {
-	if l1 == nil {
-		return l2
-	}
-
-	if l2 == nil {
-		return l1
-	}
-
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	// 这里取指针类型的主要原因是后面代码有head = head.Next
 	head := &ListNode{}
+	pHead := head
 
-	if l1.Val <= l2.Val {
-		head = l1
-		head.Next = mergeTwoLists(l1.Next, l2)
-	} else {
-		head = l2
-		head.Next = mergeTwoLists(l1, l2.Next)
+	for l1 != nil && l2 != nil {
+		if l1.Val <= l2.Val {
+			head.Next = l1
+			l1 = l1.Next
+		} else {
+			head.Next = l2
+			l2 = l2.Next
+		}
+		head = head.Next // head 是已经被确定顺序的节点，head.Next 是后续操作要被确定顺序的节点
 	}
 
-	return head
+	if l1 != nil {
+		head.Next = l1
+	}
+	if l2 != nil {
+		head.Next = l2
+	}
+
+	return pHead.Next
 }

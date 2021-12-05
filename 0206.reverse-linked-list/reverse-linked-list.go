f1634e07e87ed498514c69f2f8435db8d76af720
Xing Gao <18340825824@163.com>
2021-12-05 20:04:05 +0800

    206. 反转链表 添加注释

--- 0206.reverse-linked-list/reverse-linked-list.go
+++ 0206.reverse-linked-list/reverse-linked-list.go
@@ -5,13 +5,15 @@ import "github.com/yigenshutiao/Golang-algorithm-template/util"
 type ListNode = util.ListNode
 
 func reverseList(head *ListNode) *ListNode {
-	var prev *ListNode
-	curr := head
-	for curr != nil {
-		next := curr.Next
-		curr.Next = prev
-		prev = curr
-		curr = next
+	// 这里不能声明为 pre := &ListNode{}, 因为会初始化，这里只需要声明，不需要初始化
+	var pre *ListNode
+	cur := head
+	// head不会动，这里的变量是游标cur, 判断时应用cur进行判断
+	for cur != nil {
+		tmp := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = tmp
 	}
-	return prev
+	return pre
 }

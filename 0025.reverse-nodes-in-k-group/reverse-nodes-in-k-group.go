6995c76ca77c278553c067b5af656d33dc65ee1b
Xing Gao <18340825824@163.com>
2021-12-08 00:16:05 +0800

    25. K 个一组翻转链表

---
+++ 0025.reverse-nodes-in-k-group/reverse-nodes-in-k-group.go
@@ -0,0 +1,39 @@
+package _025_reverse_nodes_in_k_group
+
+import "github.com/yigenshutiao/Golang-algorithm-template/util"
+
+type ListNode = util.ListNode
+
+func reverseKGroup(head *ListNode, k int) *ListNode {
+	dummy := &ListNode{Next: head}
+	pre := dummy
+
+	for head != nil {
+		tail := pre
+		for i := 0; i < k; i++ {
+			tail = tail.Next
+			if tail == nil {
+				return dummy.Next
+			}
+		}
+		nex := tail.Next
+		head, tail = myReverse(head, tail)
+		pre.Next = head
+		tail.Next = nex
+		pre = tail
+		head = tail.Next
+	}
+	return dummy.Next
+}
+
+func myReverse(head, tail *ListNode) (*ListNode, *ListNode) {
+	prev := tail.Next
+	p := head
+	for prev != tail {
+		nex := p.Next
+		p.Next = prev
+		prev = p
+		p = nex
+	}
+	return tail, head
+}

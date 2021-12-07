97a0984364fc6707b90b7c939b9e54ad545abb57
gaoxing <richardgaoxing@didichuxing.com>
2021-12-07 14:22:44 +0800

    234. 回文链表

---
+++ 0234.palindrome-linked-list/palindrome-linked-list.go
@@ -0,0 +1,50 @@
+package _234_palindrome_linked_list
+
+import "github.com/yigenshutiao/Golang-algorithm-template/util"
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode = util.ListNode
+
+func traverse(head *ListNode) *ListNode {
+	var prev *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = next
+	}
+	return prev
+}
+
+func isPalindrome(head *ListNode) bool {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	if fast != nil {
+		slow = slow.Next
+	}
+
+	// 反转链表并赋给新节点
+	right := traverse(slow)
+
+	cur := head
+	for right != nil {
+		if cur.Val != right.Val {
+			return false
+		}
+		right = right.Next
+		cur = cur.Next
+	}
+
+	return true
+}

8fceddb3f81263806d47482d8e4adfb8d85cf472
gaoxing <richardgaoxing@didichuxing.com>
2021-12-28 13:40:16 +0800

    27. 移除元素 没啥卵用的题

---
+++ 0027.remove-element/remove-element.go
@@ -0,0 +1,22 @@
+package _027_remove_element
+
+func removeElement(nums []int, val int) int {
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == val {
+			for j := i + 1; j < len(nums); j++ {
+				if nums[j] != val {
+					nums[i], nums[j] = nums[j], nums[i]
+					break
+				}
+			}
+		}
+	}
+
+	res := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			res += 1
+		}
+	}
+	return res
+}

a2b992196f02a4d57c3d638dbeac5711f785a81f
Xing Gao <18340825824@163.com>
2022-01-01 16:24:28 +0800

    28. 实现 strStr()

---
+++ 0028.implement-strstr/implement-strstr.go
@@ -0,0 +1,10 @@
+package _028_implement_strstr
+
+func strStr(haystack string, needle string) int {
+	for i := 0; i <= len(haystack)-len(needle); i++ {
+		if haystack[i:i+len(needle)] == needle {
+			return i
+		}
+	}
+	return -1
+}

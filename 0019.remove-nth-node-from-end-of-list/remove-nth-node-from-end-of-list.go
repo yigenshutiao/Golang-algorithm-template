9eab8ca0f31f4eefca49bb5abc6bc5fcb7c18425
gaoxing <richardgaoxing@didichuxing.com>
2022-01-04 13:47:50 +0800

    19. 删除链表的倒数第 N 个结点

--- 0019.remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.go
+++ 0019.remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.go
@@ -8,6 +8,7 @@ type ListNode struct {
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	pre := &ListNode{0, head}
 	slow, fast := pre, pre
+	// 一共5个节点，n = 3，n+1=4，再走1步就能找到n的前置节点，这是走n+1步的原因
 	// 1. 快指针走n+1步
 	for i := 0; i <= n; i++ {
 		fast = fast.Next

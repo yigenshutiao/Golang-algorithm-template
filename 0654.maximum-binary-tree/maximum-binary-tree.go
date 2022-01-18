b6ee59506e03d752bfe7e8001f2d01fc2e53d34e
Xing Gao <18340825824@163.com>
2022-01-18 23:54:39 +0800

    654. 最大二叉树 二刷

--- 0654.maximum-binary-tree/maximum-binary-tree.go
+++ 0654.maximum-binary-tree/maximum-binary-tree.go
@@ -9,21 +9,29 @@ func constructMaximumBinaryTree(nums []int) *TreeNode {
 	if len(nums) < 1 {
 		return nil
 	}
-	//首选找到最大值
-	index := findMax(nums)
-	//其次构造二叉树
+
+	idx := getMax(nums)
+
 	root := &TreeNode{
-		Val:   nums[index],
-		Left:  constructMaximumBinaryTree(nums[:index]),   //左半边
-		Right: constructMaximumBinaryTree(nums[index+1:]), //右半边
+		Val:   nums[idx],
+		Left:  constructMaximumBinaryTree(nums[0:idx]),
+		Right: constructMaximumBinaryTree(nums[idx+1 : len(nums)]),
 	}
+
 	return root
 }
-func findMax(nums []int) (index int) {
-	for i := 0; i < len(nums); i++ {
-		if nums[i] > nums[index] {
-			index = i
+
+func getMax(nums []int) int {
+	if len(nums) <= 1 {
+		return 0
+	}
+	res := 0
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i] > nums[res] {
+			res = i
 		}
 	}
-	return
+
+	return res
 }

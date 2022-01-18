b5b3ccae45646f83f305118c28c21070d8af86e8
Xing Gao <18340825824@163.com>
2022-01-18 23:33:50 +0800

    105. 从前序与中序遍历序列构造二叉树 二刷

--- 0105.construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.go
+++ 0105.construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.go
@@ -7,24 +7,27 @@ type TreeNode = util.TreeNode
 // buildTree
 // preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
 func buildTree(preorder []int, inorder []int) *TreeNode {
-	if len(preorder) == 0 || len(inorder) == 0 {
+	if len(preorder) < 1 || len(inorder) < 1 {
 		return nil
 	}
 
-	index := findIndex(inorder, preorder[0])
+	idx := findIndex(inorder, preorder[0])
+
 	root := &TreeNode{
 		Val:   preorder[0],
-		Left:  buildTree(preorder[1:index+1], inorder[:index]),
-		Right: buildTree(preorder[index+1:], inorder[index+1:]),
+		Left:  buildTree(preorder[1:idx+1], inorder[:idx]),
+		Right: buildTree(preorder[idx+1:], inorder[idx+1:]),
 	}
+
 	return root
 }
 
 func findIndex(nums []int, target int) int {
-	for i := 0; i < len(nums); i++ {
-		if nums[i] == target {
+	for i, v := range nums {
+		if v == target {
 			return i
 		}
 	}
+
 	return -1
 }

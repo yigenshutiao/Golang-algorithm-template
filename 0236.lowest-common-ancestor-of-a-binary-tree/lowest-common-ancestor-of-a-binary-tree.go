a1f684c00b122e7fe8338a70ca409fa194c8c8aa
gaoxing <richardgaoxing@didichuxing.com>
2022-01-21 13:55:27 +0800

    236. 二叉树的最近公共祖先

--- 0236.lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.go
+++ 0236.lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.go
@@ -19,15 +19,20 @@ func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
 		return root
 	}
 
-	if left != nil {
+	if left != nil && right == nil {
 		return left
 	}
 	//当 left 为空 ，right 不为空 ：p,q 都不在 root 的左子树中，直接返回 right 。具体可分为两种情况：
 	//p,q 其中一个在 root 的 右子树 中，此时 right 指向 p（假设为 p ）；
 	//p,q 两节点都在 root 的 右子树 中，此时的 right 指向 最近公共祖先节点 ；
-	if right != nil {
+	if right != nil && left == nil {
 		return right
 	}
+
 	// 当 left 和 right 同时为空 ：说明 root 的左 / 右子树中都不包含 p,q，返回 null
+	if left == nil && right == nil {
+		return nil
+	}
+
 	return nil
 }

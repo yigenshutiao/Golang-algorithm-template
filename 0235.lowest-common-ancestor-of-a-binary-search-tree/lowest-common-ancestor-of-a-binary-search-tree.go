bf119bbfec358aabdb7f8087235125fd3893218c
gaoxing <richardgaoxing@didichuxing.com>
2022-01-21 13:55:11 +0800

    235. 二叉搜索树的最近公共祖先

---
+++ 0235.lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.go
@@ -0,0 +1,17 @@
+package _235_lowest_common_ancestor_of_a_binary_search_tree
+
+import "github.com/yigenshutiao/Golang-algorithm-template/util"
+
+type TreeNode = util.TreeNode
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val > p.Val && root.Val > q.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	} else if root.Val < p.Val && root.Val < q.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	}
+	return root
+}

9583696675d9f2f32b37bf2cf848b09a20fb83aa
Xing Gao <18340825824@163.com>
2022-01-30 11:25:34 +0800

    96. 不同的二叉搜索树

--- 0096.unique-binary-search-trees/unique-binary-search-trees.go
+++ 0096.unique-binary-search-trees/unique-binary-search-trees.go
@@ -3,13 +3,12 @@ package _096_unique_binary_search_trees
 func numTrees(n int) int {
 	dp := make([]int, n+1)
 	dp[0] = 1
-	dp[1] = 1
-	for i := 2; i <= n; i++ {
-		for j := 0; j <= i-1; j++ {
-			left := j
-			right := i - j - 1
-			dp[i] += dp[left] * dp[right]
+	// dp[3]，就是 元素1为头结点搜索树的数量 + 元素2为头结点搜索树的数量 + 元素3为头结点搜索树的数量
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			dp[i] += dp[i-j] * dp[j-1]
 		}
 	}
+
 	return dp[n]
 }

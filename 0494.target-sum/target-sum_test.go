c743ea221d82c293200350470b401f06f8a51299
Xing Gao <18340825824@163.com>
2022-02-01 18:10:57 +0800

    0494.  凑数

---
+++ 0494.target-sum/target-sum_test.go
@@ -0,0 +1,31 @@
+package _494_target_sum
+
+import "testing"
+
+func Test_findTargetSumWays(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"test 1",
+			args{
+				[]int{100},
+				-200,
+			},
+			0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findTargetSumWays(tt.args.nums, tt.args.target); got != tt.want {
+				t.Errorf("findTargetSumWays() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

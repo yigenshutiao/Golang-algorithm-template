e31e2370dbc3ce17bca60e9872153fb700a6c720
Xing Gao <18340825824@163.com>
2022-02-01 18:16:45 +0800

    22. 括号生成

---
+++ 0022.generate-parentheses/generate-parentheses.go
@@ -0,0 +1,30 @@
+package _022_generate_parentheses
+
+func generateParenthesis(n int) []string {
+	res := []string{}
+
+	if n == 0 {
+		return res
+	}
+
+	var dfs func(cur string, left, right int)
+	dfs = func(cur string, left, right int) {
+		if left == 0 && right == 0 {
+			res = append(res, cur)
+			return
+		}
+		// 这里相当于剪枝
+		if left > right {
+			return
+		}
+		if left > 0 {
+			dfs(cur+"(", left-1, right)
+		}
+		if right > 0 {
+			dfs(cur+")", left, right-1)
+		}
+	}
+
+	dfs("", n, n)
+	return res
+}
---
+++ 0022.generate-parentheses/generate-parentheses_test.go
@@ -0,0 +1,32 @@
+package _022_generate_parentheses
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_generateParenthesis(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			"test 1",
+			args{
+				2,
+			},
+			[]string{"(())", "()()"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := generateParenthesis(tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("generateParenthesis() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

510484824ec53a23c08bc1e01c33126103b8ab93
Xing Gao <18340825824@163.com>
2022-02-04 23:18:15 +0800

    543. 二叉树的直径

---
+++ 0543.diameter-of-binary-tree/diameter-of-binary-tree.go
@@ -0,0 +1,33 @@
+package _543_diameter_of_binary_tree
+
+import "github.com/yigenshutiao/Golang-algorithm-template/util"
+
+type TreeNode = util.TreeNode
+
+func diameterOfBinaryTree(root *TreeNode) int {
+	res := 1
+	var traverse func(node *TreeNode) int
+	traverse = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		}
+
+		left := traverse(node.Left)
+		right := traverse(node.Right)
+
+		res = max(res, left+right+1)
+
+		return max(left, right) + 1
+	}
+
+	traverse(root)
+
+	return res - 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}

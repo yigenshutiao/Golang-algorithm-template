7ec93e2ae690445793a9ee44bb71d48c00347ac0
Xing Gao <18340825824@163.com>
2022-02-07 15:01:10 +0800

    283. 移动零

--- 0283.move-zeroes/move-zeroes.go
+++ 0283.move-zeroes/move-zeroes.go
@@ -1,5 +1,6 @@
 package _283_move_zeroes
 
+// 双指针
 func moveZeroes(nums []int) {
 	l, r := 0, 0
 	n := len(nums)
@@ -10,5 +11,24 @@ func moveZeroes(nums []int) {
 			l++
 		}
 		r++
+	}
+}
+
+// 暴力解法
+func moveZeroes2(nums []int) {
+	if len(nums) < 2 {
+		return
+	}
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 0 {
+			t := i
+			for j := i + 1; j < len(nums); j++ {
+				if nums[j] != 0 {
+					nums[i], nums[j] = nums[j], nums[i]
+					i = j
+				}
+			}
+			i = t
+		}
 	}
 }

36e14975f976ef038ac5cb422f445312c8f8be20
gaoxing <richardgaoxing@didichuxing.com>
2022-02-10 13:04:05 +0800

    581. 最短无序连续子数组 技巧题，第二次做可能还是不会

---
+++ 0581.shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.go
@@ -0,0 +1,27 @@
+package _581_shortest_unsorted_continuous_subarray
+
+func findUnsortedSubarray(nums []int) int {
+	left, right := -1, -1
+	n := len(nums)
+	minN, maxN := 999999999, -999999999
+	for i := 0; i < len(nums); i++ {
+		// 从左往右走，应为升序，若遇到降序，记录节点，取最后一个节点作为右边端点
+		if nums[i] < maxN {
+			right = i
+		} else {
+			maxN = nums[i]
+		}
+		// 从右往左走，应为降序，若遇到升序，记录节点，取最后一个节点作为左边端点
+		if nums[n-i-1] > minN {
+			left = n - i - 1
+		} else {
+			minN = nums[n-i-1]
+		}
+	}
+
+	if right == -1 {
+		return 0
+	}
+
+	return right - left + 1
+}

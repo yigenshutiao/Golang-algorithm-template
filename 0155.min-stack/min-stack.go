ff7f78200bb53d2175b6ee83dfa54d0594a9f979
gaoxing <richardgaoxing@didichuxing.com>
2022-03-16 13:51:03 +0800

    155. 最小栈

---
+++ 0155.min-stack/min-stack.go
@@ -0,0 +1,50 @@
+package _155_min_stack
+
+import "math"
+
+type MinStack struct {
+	stack    []int
+	minStack []int // 用了一个辅助栈，栈顶元素最小
+}
+
+func Constructor() MinStack {
+	return MinStack{
+		stack:    []int{},
+		minStack: []int{math.MaxInt},
+	}
+}
+
+func (this *MinStack) Push(val int) {
+	this.stack = append(this.stack, val)
+	top := this.minStack[len(this.minStack)-1]
+	this.minStack = append(this.minStack, min(val, top))
+}
+
+func (this *MinStack) Pop() {
+	this.stack = this.stack[:len(this.stack)-1]
+	this.minStack = this.minStack[:len(this.minStack)-1]
+}
+
+func (this *MinStack) Top() int {
+	return this.stack[len(this.stack)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	return this.minStack[len(this.minStack)-1]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(val);
+ * obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.GetMin();
+ */

2f407d37967e5abd08b00b8e66706541b8d1bc83
Xing Gao <18340825824@163.com>
2022-03-31 23:38:40 +0800

    17. 电话号码的字母组合 二刷

---
+++ 0005.longest-palindromic-substring/longest-palindromic-substring_test.go
@@ -0,0 +1,29 @@
+package _005_longest_palindromic_substring
+
+import "testing"
+
+func Test_longestPalindrome(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			"test 1",
+			args{
+				"babad",
+			},
+			"aba",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestPalindrome(tt.args.s); got != tt.want {
+				t.Errorf("longestPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
--- 0017.letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.go
+++ 0017.letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.go
@@ -2,36 +2,35 @@ package _017_letter_combinations_of_a_phone_number
 
 func letterCombinations(digits string) []string {
 	var res []string
-	if len(digits) < 1 {
+	if digits == "" {
 		return res
 	}
-	kv := map[string]string{
-		"2": "abc",
-		"3": "def",
-		"4": "ghi",
-		"5": "jkl",
-		"6": "mno",
-		"7": "pqrs",
-		"8": "tuv",
-		"9": "wxyz",
+
+	digMap := map[byte]string{
+		'2': "abc",
+		'3': "def",
+		'4': "ghi",
+		'5': "jkl",
+		'6': "mno",
+		'7': "pqrs",
+		'8': "tuv",
+		'9': "wxyz",
 	}
 
-	var dfs func(res *[]string, i int, cur string)
-	dfs = func(res *[]string, i int, cur string) {
-		if len(cur) == len(digits) {
+	var dfs func(curIdx int, cur string, res *[]string)
+	dfs = func(curIdx int, cur string, res *[]string) {
+		if curIdx == len(digits) {
 			*res = append(*res, cur)
 			return
 		}
 
-		// 单层逻辑
-		k := string(digits[i])
-		for _, v := range kv[k] {
+		for _, v := range digMap[digits[curIdx]] {
 			cur = cur + string(v)
-			dfs(res, i+1, cur)
+			dfs(curIdx+1, cur, res)
 			cur = cur[:len(cur)-1]
 		}
 	}
+	dfs(0, "", &res)
 
-	dfs(&res, 0, "")
 	return res
 }

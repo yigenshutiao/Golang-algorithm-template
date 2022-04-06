438e0d4f9bb1778441b66f0c45acda49d99e4a62
gaoxing <richardgaoxing@didichuxing.com>
2022-04-06 13:10:43 +0800

    53. 最大子数组和 二刷

--- 0053.maximum-subarray/maximum-subarray.go
+++ 0053.maximum-subarray/maximum-subarray.go
@@ -8,12 +8,15 @@ func maxSubArray(nums []int) int {
 	for i := 1; i < len(dp); i++ {
 		dp[i] = -9999999999
 	}
-
+	// dp[i] 以nums[i] 为下标的最大子字符串的和
+	// 对于每个数字来说，只有两种选择：只要当前的数字，当前数字+上次最大的值
+	// 初始化dp[0]为nums[0]
 	for i := 1; i < len(nums); i++ {
 		dp[i] = max(nums[i], dp[i-1]+nums[i])
 	}
 
 	res := -999999
+	// 没办法找到最大值，再遍历一次
 	for _, num := range dp {
 		if num > res {
 			res = num

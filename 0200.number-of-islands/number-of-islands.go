de170858cfc18e4455be2b89d5b004f8d8ea993f
Xing Gao <18340825824@163.com>
2022-04-21 23:53:21 +0800

    200. 岛屿数量 二刷，图的dfs

--- 0200.number-of-islands/number-of-islands.go
+++ 0200.number-of-islands/number-of-islands.go
@@ -1,38 +1,36 @@
 package _200_number_of_islands
 
 func numIslands(grid [][]byte) int {
-	if grid == nil {
-		return 0
-	}
-
 	var res int
 
-	// len(grid) 纵坐标  len(grid[0]) 横坐标
+	var dfs func(i, j int)
+
+	dfs = func(i, j int) {
+		if i < 0 || i >= len(grid) || j < 0 || j >= len(grid[0]) {
+			return
+		}
+
+		if grid[i][j] == '1' {
+			grid[i][j] = '2'
+		} else {
+			return
+		}
+
+		// 如果这个点是岛屿，往周边蔓延，染色所有岛屿
+		dfs(i-1, j)
+		dfs(i+1, j)
+		dfs(i, j-1)
+		dfs(i, j+1)
+	}
+
 	for i := 0; i < len(grid); i++ {
 		for j := 0; j < len(grid[0]); j++ {
 			if grid[i][j] == '1' {
-				res += 1
-				dfs(grid, i, j)
+				res++
+				dfs(i, j)
 			}
 		}
 	}
 
 	return res
-}
-
-func dfs(grid [][]byte, i, j int) {
-	if i < 0 || j < 0 || i >= len(grid) || j >= len(grid[0]) {
-		return
-	}
-
-	if grid[i][j] == '1' {
-		grid[i][j] = '2'
-	} else {
-		return
-	}
-
-	dfs(grid, i+1, j)
-	dfs(grid, i-1, j)
-	dfs(grid, i, j-1)
-	dfs(grid, i, j+1)
 }
---
+++ 0200.number-of-islands/number-of-islands_test.go
@@ -0,0 +1,31 @@
+package _200_number_of_islands
+
+import (
+	"testing"
+)
+
+func Test_numIslands(t *testing.T) {
+	type args struct {
+		grid [][]byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"test1",
+			args{
+				[][]byte{{'1', '1', '0', '0', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '1', '0', '0'}, {'0', '0', '0', '1', '1'}},
+			},
+			3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numIslands(tt.args.grid); got != tt.want {
+				t.Errorf("numIslands() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

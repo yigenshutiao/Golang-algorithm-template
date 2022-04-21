ae3115e4d2fa265801ee52e44e3ac118e7088baa
Xing Gao <18340825824@163.com>
2022-04-22 00:14:15 +0800

    198. 打家劫舍 二刷

--- 0198.house-robber/house-robber.go
+++ 0198.house-robber/house-robber.go
@@ -2,29 +2,32 @@ package _198_house_robber
 
 //input [2,7,9,3,1]
 func rob(nums []int) int {
-	if len(nums) == 2 {
-		return max(nums[0], nums[1])
-	}
-	dp := make([]int, len(nums)+1)
-	dp[0] = 0
-	for i := 0; i < len(nums); i++ {
-		if i == 0 {
-			dp[1] = nums[0]
-			continue
-		}
-		if i == 1 {
-			dp[2] = max(dp[0], nums[i])
-		}
+	dp := make([][]int, len(nums))
 
-		dp[i+1] = max(dp[i], dp[i-1]+nums[i])
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, 2)
 	}
 
-	return dp[len(nums)]
+	// [0]代表不偷， [1]代表偷
+	dp[0][0] = 0
+	dp[0][1] = nums[0]
+
+	for i := 1; i < len(nums); i++ {
+		// 今天偷：昨天不偷，今天偷; 昨天偷
+		dp[i][1] = max(dp[i-1][0]+nums[i], dp[i-1][1])
+		// 今天不偷：昨天偷了, 昨天不偷
+		dp[i][0] = max(dp[i-1][1], dp[i-1][0])
+	}
+
+	if dp[len(nums)-1][0] > dp[len(nums)-1][1] {
+		return dp[len(nums)-1][0]
+	}
+	return dp[len(nums)-1][1]
 }
 
 func max(a, b int) int {
-	if a > b {
-		return a
+	if a < b {
+		return b
 	}
-	return b
+	return a
 }

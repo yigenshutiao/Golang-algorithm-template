8d700eb7257d6233052cd3755bf3f78bba3f0c75
Xing Gao <18340825824@163.com>
2022-04-21 23:54:13 +0800

    200. 岛屿数量 二刷，图的dfs

---
+++ 0148.sort-list/sort-list_test.go
@@ -0,0 +1,33 @@
+package _148_sort_list
+
+import (
+	"github.com/yigenshutiao/Golang-algorithm-template/util"
+	"reflect"
+	"testing"
+)
+
+func Test_sortList(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			"test1",
+			args{
+				head: util.Ints2List([]int{4, 1, 3, 2}),
+			},
+			util.Ints2List([]int{1, 2, 3, 4}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sortList(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sortList() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

0d330ee8c40bac1e399fbda9bddcce027526d10d
Xing Gao <18340825824@163.com>
2022-04-23 20:09:14 +0800

    215. 数组中的第K个最大元素 二刷

--- 0215.kth-largest-element-in-an-array/kth-largest-element-in-an-array.go
+++ 0215.kth-largest-element-in-an-array/kth-largest-element-in-an-array.go
@@ -1,30 +1,31 @@
 package _215_kth_largest_element_in_an_array
 
 func findKthLargest(nums []int, k int) int {
-	kk := len(nums) - k
+	// 找k大的，就是找pos小的
+	resPos := len(nums) - k
 	left, right := 0, len(nums)-1
-
 	for {
-		pos := partition(nums, left, right)
-		if pos == kk {
-			return nums[kk]
-		} else if pos > kk {
+		pos := getPos(nums, left, right)
+		if pos == resPos {
+			return nums[pos]
+		} else if pos > resPos {
+			// 如果找到的位置在结果的右边, 说明答案在左边，往左边收缩
 			right = pos - 1
-		} else if pos < kk {
+		} else if pos < resPos {
+			// 如果找到位置在pos左边，说明答案在右边，往右边收缩
 			left = pos + 1
 		}
 	}
 }
 
-func partition(nums []int, left, right int) int {
-	pivot := left
-	pos := right
-	for i := right; i >= left; i-- {
-		if nums[i] > nums[pivot] {
+func getPos(nums []int, left, right int) int {
+	idx, pos := left, right
+	for i := right; i > left; i-- {
+		if nums[i] > nums[idx] {
 			nums[i], nums[pos] = nums[pos], nums[i]
 			pos--
 		}
 	}
-	nums[pos], nums[pivot] = nums[pivot], nums[pos]
+	nums[pos], nums[idx] = nums[idx], nums[pos]
 	return pos
 }

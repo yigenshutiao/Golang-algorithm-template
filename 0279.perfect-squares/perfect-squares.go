3b4bee838fa9fb6bae49d3c3bf0cb9dd1163638f
gaoxing <richardgaoxing@didichuxing.com>
2022-04-26 13:13:09 +0800

    279. 完全平方数

--- 0279.perfect-squares/perfect-squares.go
+++ 0279.perfect-squares/perfect-squares.go
@@ -3,16 +3,17 @@ package _279_perfect_squares
 func numSquares(n int) int {
 	dp := make([]int, n+1)
 	for i := 0; i < len(dp); i++ {
-		dp[i] = 999999999
+		dp[i] = 999999
 	}
-
+	// 和为0的完全平方数为0
 	dp[0] = 0
 
-	for i := 0; i <= n; i++ {
+	for i := 1; i <= n; i++ {
 		for j := 1; j*j <= i; j++ {
 			dp[i] = min(dp[i], dp[i-j*j]+1)
 		}
 	}
+
 	return dp[n]
 }
 
@@ -20,6 +21,5 @@ func min(a, b int) int {
 	if a < b {
 		return a
 	}
-
 	return b
 }
---
+++ 0279.perfect-squares/perfect-squares_test.go
@@ -0,0 +1,29 @@
+package _279_perfect_squares
+
+import "testing"
+
+func Test_numSquares(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"test1",
+			args{
+				13,
+			},
+			2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numSquares(tt.args.n); got != tt.want {
+				t.Errorf("numSquares() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

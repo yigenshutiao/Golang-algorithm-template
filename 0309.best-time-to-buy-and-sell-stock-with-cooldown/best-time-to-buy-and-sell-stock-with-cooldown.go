2f90a6c6106a02c7ba0ba7a3e9c711a79345249b
gaoxing <richardgaoxing@didichuxing.com>
2022-04-29 13:44:44 +0800

    309. 最佳买卖股票时机含冷冻期

--- 0309.best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.go
+++ 0309.best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.go
@@ -7,22 +7,23 @@ func maxProfit(prices []int) int {
 		dp[i] = make([]int, 2)
 	}
 
+	// 第一个元素是天数，第二个元素，0没有股票，1代表有股票
 	dp[0][0] = 0
 	dp[0][1] = -prices[0]
 
-	for i := 0; i < len(prices); i++ {
-		if i == 0 {
-			dp[i][0] = 0
-			dp[i][1] = -prices[0]
-			continue
-		}
+	for i := 1; i < len(prices); i++ {
 
 		if i == 1 {
+			// 第一天的话，不持有股票： 前一天不持有股票，前一天持有股票，今天卖了
 			dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+			// 持有股票：前一天持有股票，前一天不持有股票，今天买
 			dp[i][1] = max(dp[i-1][1], -prices[i])
 			continue
 		}
+
+		// 今天不持有股票：昨天不持有股票，昨天持有股票，今天卖
 		dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+		// 今天持有股票：昨天持有股票，前天不持有股票，昨天冷冻期，今天买
 		dp[i][1] = max(dp[i-1][1], dp[i-2][0]-prices[i])
 	}
 	return dp[len(prices)-1][0]

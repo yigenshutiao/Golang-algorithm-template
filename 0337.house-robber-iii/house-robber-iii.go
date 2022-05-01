7b93159a07ff1a94a6904f67a08dd440af4a2371
Xing Gao <18340825824@163.com>
2022-05-01 13:06:53 +0800

    337. 打家劫舍III

--- 0337.house-robber-iii/house-robber-iii.go
+++ 0337.house-robber-iii/house-robber-iii.go
@@ -5,6 +5,8 @@ import "github.com/yigenshutiao/Golang-algorithm-template/util"
 type TreeNode = util.TreeNode
 
 func rob(root *TreeNode) int {
+	// dp数组就两个元素，一个是0代表偷当前节点的最大值，1代表不偷当前节点的最大值
+	// 当前节点依赖子节点，所以遍历顺序是后续遍历
 	val := dfs(root)
 	// 0代表选择当前节点，1代表不选择当前节点
 	return max(val[0], val[1])
@@ -16,7 +18,9 @@ func dfs(root *TreeNode) []int {
 	}
 
 	left, right := dfs(root.Left), dfs(root.Right)
+	// 偷当前节点，左右孩子就不能偷
 	choose := root.Val + left[1] + right[1]
+	// 不偷当前节点，可以任意选择左右孩子的最大值
 	notChoose := max(left[0], left[1]) + max(right[0], right[1])
 
 	return []int{choose, notChoose}
---
+++ 0337.house-robber-iii/house-robber-iii_test.go
@@ -0,0 +1,81 @@
+package _337_house_robber_iii
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// tcs is testcase slice
+var tcs = []struct {
+	nums []int
+	ans  int
+}{
+
+	// 用 0 表示 nil
+	//{
+	//	[]int{4, 1, 0, 2, 0, 3},
+	//	7,
+	//},
+
+	{
+		[]int{3, 2, 3, 0, 3, 0, 1},
+		7,
+	},
+
+	{
+		[]int{3, 4, 5, 1, 3, 0, 1},
+		9,
+	},
+
+	// 可以有多个 testcase
+}
+
+func Test_rob(t *testing.T) {
+	ast := assert.New(t)
+
+	for _, tc := range tcs {
+		fmt.Printf("~~%v~~\n", tc)
+		root := &TreeNode{}
+		initTree(tc.nums, []*TreeNode{}, root)
+		ast.Equal(tc.ans, rob(root), "输入:%v", tc)
+	}
+}
+
+func initTree(nums []int, parents []*TreeNode, root *TreeNode) {
+	if len(nums) == 0 {
+		return
+	}
+
+	if len(parents) == 0 {
+		if nums[0] != 0 {
+			root.Val = nums[0]
+			parents = append(parents, root)
+			initTree(nums[1:], parents, root)
+		}
+
+		return
+	}
+
+	newParents := make([]*TreeNode, 0, len(parents)*2)
+	for _, parent := range parents {
+
+		if nums[0] != 0 {
+			parent.Left = &TreeNode{Val: nums[0]}
+			newParents = append(newParents, parent.Left)
+		}
+
+		if len(nums) == 1 {
+			return
+		}
+
+		if nums[1] != 0 {
+			parent.Right = &TreeNode{Val: nums[1]}
+			newParents = append(newParents, parent.Right)
+		}
+
+		nums = nums[2:]
+	}
+
+	initTree(nums, newParents, root)
+}

430566ea683fa2f4625d0f839c3c782d24ed6964
Xing Gao <18340825824@163.com>
2022-05-09 23:11:55 +0800

    300. 最长递增子序列

--- 0300.longest-increasing-subsequence/longest-increasing-subsequence.go
+++ 0300.longest-increasing-subsequence/longest-increasing-subsequence.go
@@ -1,24 +1,23 @@
 package _300_longest_increasing_subsequence
 
 func lengthOfLIS(nums []int) int {
-	dp := make([]int, len(nums))
+	var res int
 
-	for i := 0; i < len(nums); i++ {
+	dp := make([]int, len(nums)+1)
+	for i := 0; i < len(dp); i++ {
 		dp[i] = 1
 	}
 
-	for i := 1; i < len(nums); i++ {
-		for j := 0; j < i; j++ {
-			// 如果nums[i] 比 nums[j] 大
-			if nums[i] > nums[j] {
-				dp[i] = max(dp[i], dp[j]+1)
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[j] > nums[i] {
+				dp[j] = max(dp[j], dp[i]+1)
 			}
 		}
 	}
 
-	var res int
 	for i := 0; i < len(dp); i++ {
-		if dp[i] > res {
+		if res < dp[i] {
 			res = dp[i]
 		}
 	}

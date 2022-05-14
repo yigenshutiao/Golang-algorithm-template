729f76c05e7b7b7c711e43be1e0c1dc4532afd01
Xing Gao <18340825824@163.com>
2022-05-14 15:03:48 +0800

    581. 最短无序连续子数组

--- 0581.shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.go
+++ 0581.shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.go
@@ -4,18 +4,19 @@ func findUnsortedSubarray(nums []int) int {
 	left, right := -1, -1
 	n := len(nums)
 	minN, maxN := 999999999, -999999999
-	for i := 0; i < len(nums); i++ {
-		// 从左往右走，应为升序，若遇到降序，记录节点，取最后一个节点作为右边端点
+	for i := 0; i < n; i++ {
+		// 从左往右走，应为升序(或者平)，若遇到降序，记录节点，取最后一个节点作为右边端点
 		if nums[i] < maxN {
 			right = i
 		} else {
 			maxN = nums[i]
 		}
-		// 从右往左走，应为降序，若遇到升序，记录节点，取最后一个节点作为左边端点
-		if nums[n-i-1] > minN {
-			left = n - i - 1
+		// 从右往左走，应为降序(或者平)，若遇到升序，记录节点，取最后一个节点作为左边端点
+		x := n - i - 1
+		if nums[x] > minN {
+			left = x
 		} else {
-			minN = nums[n-i-1]
+			minN = nums[x]
 		}
 	}
 

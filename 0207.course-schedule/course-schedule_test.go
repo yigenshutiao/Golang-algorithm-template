0205932647bf88b4071f3111a999f2e4a671b36e
Xing Gao <18340825824@163.com>
2022-05-28 23:40:37 +0800

    0207.课程表

--- 0207.course-schedule/course-schedule_test.go
+++ 0207.course-schedule/course-schedule_test.go
@@ -23,7 +23,7 @@ func Test_canFinish(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := canFinishs(tt.args.numCourses, tt.args.prerequisites); got != tt.want {
+			if got := canFinish(tt.args.numCourses, tt.args.prerequisites); got != tt.want {
 				t.Errorf("canFinish() = %v, want %v", got, tt.want)
 			}
 		})

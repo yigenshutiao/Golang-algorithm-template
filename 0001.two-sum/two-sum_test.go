b04ae80a420ad4cffd7eaf2f2bb33404b38bf28e
Xing Gao <18340825824@163.com>
2022-06-04 22:09:24 +0800

    1. 两数之和

---
+++ 0001.two-sum/two-sum_test.go
@@ -0,0 +1,34 @@
+package _001_two_sum
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_twoSum(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			"test1",
+			args{
+				[]int{2, 7, 11, 15},
+				9,
+			},
+			[]int{0, 1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := twoSum(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("twoSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

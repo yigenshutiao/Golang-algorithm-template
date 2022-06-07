995f46195e5a34b5d9c801399f052b3713834ced
Xing Gao <18340825824@163.com>
2022-06-07 16:16:15 +0800

    20. 有效的括号

--- 0020.valid-parentheses/valid-parentheses_test.go
+++ 0020.valid-parentheses/valid-parentheses_test.go
@@ -21,7 +21,7 @@ func Test_isValids(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := isValids(tt.args.s); got != tt.want {
+			if got := isValid(tt.args.s); got != tt.want {
 				t.Errorf("isValids() = %v, want %v", got, tt.want)
 			}
 		})

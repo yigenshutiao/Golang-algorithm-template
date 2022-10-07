0540bd79377563d8108e577d77a1be96794fe511
Xing Gao <18340825824@163.com>
2022-10-07 20:55:29 +0800

    206. 反转链表

--- 0206. 反转链表/reverse-linked-list.go
+++ 0206. 反转链表/reverse-linked-list.go
@@ -7,15 +7,14 @@ type ListNode = util.ListNode
 func reverseList(head *ListNode) *ListNode {
 	// 这里不能声明为 pre := &ListNode{}, 因为会初始化，这里只需要声明，不需要初始化
 	var pre *ListNode
-	// 当前值要声明到head上...
-	cur := head
-	// head不会动，这里的变量是游标cur, 判断时应用cur进行判断
-	for cur != nil {
-		tmp := cur.Next
-		cur.Next = pre
-		pre = cur
-		cur = tmp
+
+	for head != nil {
+		next := head.Next
+		head.Next = pre
+		pre = head
+		head = next
 	}
+
 	return pre
 }
 

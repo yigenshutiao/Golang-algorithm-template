f3313ed3ce2eed18bc158afab45824d4fdbebcba
Xing Gao <18340825824@163.com>
2022-10-08 20:38:24 +0800

    5. 最长回文子串

--- 0005.longest-palindromic-substring/longest-palindromic-substring.go
+++ 0005.longest-palindromic-substring/longest-palindromic-substring.go
@@ -1,28 +1,26 @@
 package _005_longest_palindromic_substring
 
 func longestPalindrome(s string) string {
-	t := 0
-	res := ""
-	dp := make([][]bool, len(s))
+	var res string
 
+	dp := make([][]bool, len(s))
 	for i := 0; i < len(s); i++ {
 		dp[i] = make([]bool, len(s))
 	}
+
 	// 从下往上，从左往右
 	for i := len(s) - 1; i >= 0; i-- {
 		for j := i; j < len(s); j++ {
 			if s[i] == s[j] {
 				if j-i <= 1 {
 					dp[i][j] = true
-					if j-i+1 > t {
-						t = j - i + 1
+					if j-i+1 > len(res) {
 						res = s[i : j+1]
 					}
 				} else {
 					if dp[i+1][j-1] == true {
 						dp[i][j] = true
-						if j-i+1 > t {
-							t = j - i + 1
+						if j-i+1 > len(res) {
 							res = s[i : j+1]
 						}
 					}
@@ -30,5 +28,6 @@ func longestPalindrome(s string) string {
 			}
 		}
 	}
+
 	return res
 }

be5a7ad1fa5f9d3b8f8e85eddddebc257cbd9aed
Xing Gao <18340825824@163.com>
2022-11-02 21:58:25 +0800

    234. 回文链表 简化解法

--- 0234.palindrome-linked-list/palindrome-linked-list.go
+++ 0234.palindrome-linked-list/palindrome-linked-list.go
@@ -12,39 +12,35 @@ import "github.com/yigenshutiao/Golang-algorithm-template/util"
 
 type ListNode = util.ListNode
 
-func traverse(head *ListNode) *ListNode {
-	var prev *ListNode
-
-	for head != nil {
-		next := head.Next
-		head.Next = prev
-		prev = head
-		head = next
-	}
-	return prev
-}
-
 func isPalindrome(head *ListNode) bool {
-	slow, fast := head, head
+	fast, slow := head, head
 	for fast != nil && fast.Next != nil {
 		slow = slow.Next
 		fast = fast.Next.Next
 	}
-	if fast != nil {
-		slow = slow.Next
-	}
 
-	// 反转链表并赋给新节点
 	right := traverse(slow)
 
-	cur := head
+	newCur := head
 	for right != nil {
-		if cur.Val != right.Val {
+		if newCur.Val != right.Val {
 			return false
 		}
 		right = right.Next
-		cur = cur.Next
+		newCur = newCur.Next
 	}
 
 	return true
+
+}
+
+func traverse(head *ListNode) *ListNode {
+	var pre *ListNode
+	for head != nil {
+		next := head.Next
+		head.Next = pre
+		pre = head
+		head = next
+	}
+	return pre
 }

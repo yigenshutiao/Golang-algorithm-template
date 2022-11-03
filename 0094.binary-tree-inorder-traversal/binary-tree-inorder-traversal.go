fff3e00d97a39993ca323655e08e411ea88e6967
Xing Gao <18340825824@163.com>
2022-11-03 10:56:42 +0800

    94. 二叉树的中序遍历

--- 0094.binary-tree-inorder-traversal/binary-tree-inorder-traversal.go
+++ 0094.binary-tree-inorder-traversal/binary-tree-inorder-traversal.go
@@ -19,3 +19,18 @@ func inorderTraversal(root *TreeNode) []int {
 	dfs(root)
 	return res
 }
+
+func inorderTraversalTT(root *TreeNode) (res []int) {
+	stack := []*TreeNode{}
+	for root != nil || len(stack) > 0 {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, root.Val)
+		root = root.Right
+	}
+	return
+}

4fc2964d0ff2cd22c7d758de803f6a571929e731
Xing Gao <18340825824@163.com>
2022-11-06 22:55:24 +0800

    22. 有效括号

--- 0022.generate-parentheses/generate-parentheses_test.go
+++ 0022.generate-parentheses/generate-parentheses_test.go
@@ -24,7 +24,7 @@ func Test_generateParenthesis(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := generateParenthesis(tt.args.n); !reflect.DeepEqual(got, tt.want) {
+			if got := generateParenthesi(tt.args.n); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("generateParenthesis() = %v, want %v", got, tt.want)
 			}
 		})

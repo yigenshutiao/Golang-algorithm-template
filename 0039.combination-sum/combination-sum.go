56567bfef585a615ebe2621afac668ba981fbc21
Xing Gao <18340825824@163.com>
2022-11-06 23:00:08 +0800

    39. 组合总和

--- 0039.combination-sum/combination-sum.go
+++ 0039.combination-sum/combination-sum.go
@@ -3,29 +3,28 @@ package _039_combination_sum
 func combinationSum(candidates []int, target int) [][]int {
 	var res [][]int
 
-	var dfs func(cur int, curidx int, tmp []int, res *[][]int)
-	dfs = func(cur int, curidx int, tmp []int, res *[][]int) {
-		if cur == 0 {
-			// 这里为啥要copy：slice是引用类型，如果不复制，那后面的循环会更新值
-			t := make([]int, len(tmp))
-			copy(t, tmp)
-			*res = append(*res, t)
-		}
-
-		if cur < 0 {
+	var dfs func(cur []int, target int, idx int)
+	dfs = func(cur []int, target int, idx int) {
+		if target == 0 {
+			tmp := make([]int, len(cur))
+			copy(tmp, cur)
+			res = append(res, tmp)
 			return
 		}
 
-		for i := curidx; i < len(candidates); i++ {
-			target -= candidates[i]
-			tmp = append(tmp, candidates[i])
-			dfs(target, i, tmp, res)
-			tmp = tmp[:len(tmp)-1]
-			target += candidates[i]
+		if target < 0 {
+			return
+		}
+
+		// 当前处理函数
+		for i := idx; i < len(candidates); i++ {
+			cur = append(cur, candidates[i])
+			dfs(cur, target-candidates[i], i)
+			cur = cur[:len(cur)-1]
 		}
 	}
 
-	dfs(target, 0, []int{}, &res)
+	dfs([]int{}, target, 0)
 
 	return res
 }

4a85544c9b52bf7265d3451958f47330b65df39d
Xing Gao <18340825824@163.com>
2022-11-08 14:39:55 +0800

    198. 打家劫舍

--- 0198.house-robber/house-robber.go
+++ 0198.house-robber/house-robber.go
@@ -8,26 +8,29 @@ func rob(nums []int) int {
 		dp[i] = make([]int, 2)
 	}
 
-	// [0]代表不偷， [1]代表偷
-	dp[0][0] = 0
-	dp[0][1] = nums[0]
+	// 偷
+	dp[0][0] = nums[0]
+	// 不偷
+	dp[0][1] = 0
 
 	for i := 1; i < len(nums); i++ {
-		// 今天偷：昨天不偷，今天偷; 昨天偷
-		dp[i][1] = max(dp[i-1][0]+nums[i], dp[i-1][1])
-		// 今天不偷：昨天偷了, 昨天不偷
-		dp[i][0] = max(dp[i-1][1], dp[i-1][0])
+		// 今天偷
+		dp[i][0] = max(dp[i-1][1]+nums[i], dp[i-1][0])
+		// 今天不偷：昨天不偷，昨天偷
+		dp[i][1] = max(dp[i-1][1], dp[i-1][0])
 	}
 
-	if dp[len(nums)-1][0] > dp[len(nums)-1][1] {
+	if dp[len(nums)-1][1] > dp[len(nums)-1][0] {
+		return dp[len(nums)-1][1]
+	} else {
 		return dp[len(nums)-1][0]
 	}
-	return dp[len(nums)-1][1]
 }
 
 func max(a, b int) int {
 	if a < b {
 		return b
 	}
+
 	return a
 }

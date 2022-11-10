0af4c3d945080c991dcf51580836f5e0ce985397
Xing Gao <18340825824@163.com>
2022-11-10 14:10:17 +0800

    72. 编辑距离

--- 0072.edit-distance/edit-distance.go
+++ 0072.edit-distance/edit-distance.go
@@ -3,25 +3,26 @@ package _072_edit_distance
 func minDistance(word1 string, word2 string) int {
 	m, n := len(word1), len(word2)
 
+	// dp[i][j] 表示以下标i-1为结尾的字符串word1，和以下标j-1为结尾的字符串word2，最近编辑距离为dp[i][j]
 	dp := make([][]int, m+1)
-	for i := 0; i < m+1; i++ {
+	for i := 0; i <= m; i++ {
 		dp[i] = make([]int, n+1)
 	}
 
-	for i := 1; i < m+1; i++ {
+	for i := 0; i <= m; i++ {
 		dp[i][0] = i
 	}
 
-	for i := 1; i < n+1; i++ {
+	for i := 0; i <= n; i++ {
 		dp[0][i] = i
 	}
 
-	for i := 1; i < m+1; i++ {
-		for j := 1; j < n+1; j++ {
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
 			if word1[i-1] == word2[j-1] {
 				dp[i][j] = dp[i-1][j-1]
 			} else {
-				dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
+				dp[i][j] = min(min(dp[i-1][j-1], dp[i][j-1]), dp[i-1][j]) + 1
 			}
 		}
 	}
@@ -29,14 +30,10 @@ func minDistance(word1 string, word2 string) int {
 	return dp[m][n]
 }
 
-func min(args ...int) int {
-	mins := args[0]
-
-	for i := 1; i < len(args); i++ {
-		if args[i] < mins {
-			mins = args[i]
-		}
+func min(a, b int) int {
+	if a < b {
+		return a
 	}
 
-	return mins
+	return b
 }

2c1bb982b1f25c2517bd8cbf0a5ae82bb15160b0
Xing Gao <18340825824@163.com>
2022-11-13 10:14:48 +0800

    279. 完全平方数 简化代码

--- 0279.perfect-squares/perfect-squares.go
+++ 0279.perfect-squares/perfect-squares.go
@@ -2,13 +2,10 @@ package _279_perfect_squares
 
 func numSquares(n int) int {
 	dp := make([]int, n+1)
-	for i := 0; i < len(dp); i++ {
-		dp[i] = 999999
-	}
-	// 和为0的完全平方数为0
-	dp[0] = 0
 
+	dp[0] = 0
 	for i := 1; i <= n; i++ {
+		dp[i] = i + 1
 		for j := 1; j*j <= i; j++ {
 			dp[i] = min(dp[i], dp[i-j*j]+1)
 		}
@@ -21,21 +18,6 @@ func min(a, b int) int {
 	if a < b {
 		return a
 	}
+
 	return b
-}
-
-func numSquare(n int) int {
-	dp := make([]int, n+1)
-	for i := 0; i <= n; i++ {
-		dp[i] = 99999999
-	}
-
-	dp[0] = 0
-	for i := 1; i <= n; i++ {
-		for j := 1; j*j <= i; j++ {
-			dp[i] = min(dp[i], dp[i-j*j]+1)
-		}
-	}
-
-	return dp[n]
 }

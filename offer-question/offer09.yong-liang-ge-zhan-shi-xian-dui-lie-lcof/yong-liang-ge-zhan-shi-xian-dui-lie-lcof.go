19465e6f77c548e0b84cb60b1659d667ba8e92d1
Xing Gao <18340825824@163.com>
2023-01-13 15:28:56 +0800

    剑指 Offer 09. 用两个栈实现队列

---
+++ offer-question/offer09.yong-liang-ge-zhan-shi-xian-dui-lie-lcof/yong-liang-ge-zhan-shi-xian-dui-lie-lcof.go
@@ -0,0 +1,32 @@
+package offer09_yong_liang_ge_zhan_shi_xian_dui_lie_lcof
+
+type CQueue struct {
+	inStack, outStack []int
+}
+
+func Constructor() CQueue {
+	return CQueue{}
+}
+
+func (this *CQueue) AppendTail(value int) {
+	this.inStack = append(this.inStack, value)
+}
+
+func (this *CQueue) DeleteHead() int {
+	if len(this.outStack) == 0 {
+		if len(this.inStack) == 0 {
+			return -1
+		}
+		this.in2out()
+	}
+	value := this.outStack[len(this.outStack)-1]
+	this.outStack = this.outStack[:len(this.outStack)-1]
+	return value
+}
+
+func (this *CQueue) in2out() {
+	for len(this.inStack) > 0 {
+		this.outStack = append(this.outStack, this.inStack[len(this.inStack)-1])
+		this.inStack = this.inStack[:len(this.inStack)-1]
+	}
+}

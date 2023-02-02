ccf14f25aa38e25e2d0941799ab95fea358ace5b
Xing Gao <18340825824@163.com>
2023-02-02 21:20:05 +0800

    42. 接雨水

--- 0042.trapping-rain-water/trapping-rain-water.go
+++ 0042.trapping-rain-water/trapping-rain-water.go
@@ -1,36 +1,5 @@
 package _042_trapping_rain_water
 
-// trap 按照列求每个雨水块的面积，优点是只需要计算高度即可
-func trap(height []int) int {
-	var res int
-
-	for i := 1; i < len(height)-1; i++ {
-		left, right := height[i], height[i]
-		// 左边界高度，左边第一个大于其高度的
-		for l := i - 1; l >= 0; l-- {
-			if height[l] > left {
-				left = height[l]
-			}
-		}
-
-		// 右边界高度，右边第一个大于其高度的
-		for r := i + 1; r <= len(height)-1; r++ {
-			if height[r] > right {
-				right = height[r]
-			}
-		}
-
-		// 求出最小值
-		h := min(left, right)
-		s := h - height[i]
-
-		// 加到面积中
-		res += s
-	}
-
-	return res
-}
-
 func min(a, b int) int {
 	if a < b {
 		return a
@@ -39,7 +8,7 @@ func min(a, b int) int {
 }
 
 // trapDP 接雨水dp解法
-func trapDP(height []int) int {
+func trap(height []int) int {
 	var res int
 
 	maxLeft := make([]int, len(height))
--- 0042.trapping-rain-water/trapping-rain-water_test.go
+++ 0042.trapping-rain-water/trapping-rain-water_test.go
@@ -21,7 +21,7 @@ func Test_traps(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := trap(tt.args.height); got != tt.want {
+			if got := trapDP(tt.args.height); got != tt.want {
 				t.Errorf("traps() = %v, want %v", got, tt.want)
 			}
 		})

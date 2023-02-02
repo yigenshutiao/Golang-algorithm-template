6ba92581309f66cfca3d4e98f093559bf888684d
Xing Gao <18340825824@163.com>
2023-02-02 15:08:50 +0800

    1. 两数之和

--- 0001.two-sum/two-sum.go
+++ 0001.two-sum/two-sum.go
@@ -2,10 +2,9 @@ package _001_two_sum
 
 func twoSum(nums []int, target int) []int {
 	for left := 0; left < len(nums); left++ {
-		t := target - nums[left]
 		right := len(nums) - 1
 		for right > left {
-			if nums[right] == t {
+			if nums[right] == target-nums[left] {
 				return []int{left, right}
 			}
 			right--

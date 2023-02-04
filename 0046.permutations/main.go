9ee0620b80b79abe480570e940fe65eebc178f1c
Xing Gao <18340825824@163.com>
2023-02-04 10:19:25 +0800

    删掉麻烦的解法

--- 0046.permutations/main.go
+++ 0046.permutations/main.go
@@ -32,36 +32,37 @@ func permute(nums []int) [][]int {
 	return res
 }
 
-func permutes(nums []int) [][]int {
-	var res [][]int
-	n := len(nums)
-	isUsed := make([]bool, n)
-	tmp := make([]int, n)
-
-	var dfs func(nums []int, isUsed []bool, cur []int, i int, res *[][]int)
-
-	dfs = func(nums []int, isUsed []bool, cur []int, idx int, res *[][]int) {
-		if idx == n {
-			t := make([]int, n)
-			copy(t, cur)
-			*res = append(*res, t)
-			return
-		}
-
-		for i := 0; i < len(nums); i++ {
-			if isUsed[i] == false {
-				cur[idx] = nums[i]
-				isUsed[i] = true
-				dfs(nums, isUsed, cur, idx+1, res)
-				isUsed[i] = false
-			}
-		}
-	}
-
-	dfs(nums, isUsed, tmp, 0, &res)
-
-	return res
-}
+//
+//func permutes(nums []int) [][]int {
+//	var res [][]int
+//	n := len(nums)
+//	isUsed := make([]bool, n)
+//	tmp := make([]int, n)
+//
+//	var dfs func(nums []int, isUsed []bool, cur []int, i int, res *[][]int)
+//
+//	dfs = func(nums []int, isUsed []bool, cur []int, idx int, res *[][]int) {
+//		if idx == n {
+//			t := make([]int, n)
+//			copy(t, cur)
+//			*res = append(*res, t)
+//			return
+//		}
+//
+//		for i := 0; i < len(nums); i++ {
+//			if isUsed[i] == false {
+//				cur[idx] = nums[i]
+//				isUsed[i] = true
+//				dfs(nums, isUsed, cur, idx+1, res)
+//				isUsed[i] = false
+//			}
+//		}
+//	}
+//
+//	dfs(nums, isUsed, tmp, 0, &res)
+//
+//	return res
+//}
 
 func main() {
 	fmt.Println(permute([]int{1, 2, 3}))
